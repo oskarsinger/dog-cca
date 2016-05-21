148d5f8cafdd3242798d72836147550f7f57ae53
oskarsinger <oskarsinger@gmail.com>
2016-05-21 19:04:25 -0400

    New initializer

--- src/python/initialize.py
+++ src/python/initialize.py
@@ -2,17 +2,43 @@ import os
 
 from subprocess import call
 
-def init_path():
+def augment_python_path(base, names):
 
-    cwd = os.getcwd()
-    dir_items = os.listdir(cwd)
+    for name in names:
 
-    for dir_item in dir_items:
-
-        full = os.path.join(cwd, dir_item)
-        print full
+        full = os.path.join(base, name)
 
         if os.path.isdir(full):
             call([
                 "export",
                 "PYTHONPATH=\"${PYTHONPATH}:" + full + "\""])
+
+def add_modules(codebase_path):
+
+    cwd = os.getcwd()
+    cwd_modules = os.listdir(cwd)
+    codebase_modules = os.listdir(codebase_path)
+
+    augment_python_path(cwd, cwd_modules)
+    augment_python_path(codebase_path, codebase_modules)
+
+def add_plot_path(plot_path):
+
+    print "Some stuff"
+
+def main():
+
+    paths = {}
+
+    with open('config.txt') as f:
+
+        pairs = [line.split(': ')
+                 for line in f]
+        paths = {name : path[:-1]
+                 for first, second in pairs}
+
+    init_python_path(paths['codebase'])
+    add_plot_path(paths['plots'])
+
+if __name__=='__main__':
+    main()
