d3fb275b45fa53815b71790504edba83ca25fb3e
oskarsinger <oskarsinger@gmail.com>
2016-05-24 14:20:42 -0400

    Improved experiment code

--- src/python/experiments/e4/online.py
+++ src/python/experiments/e4/online.py
@@ -6,49 +6,42 @@ from data.loaders import readers
 from data.servers.minibatch import Minibatch2Minibatch as M2M
 from drrobert.arithmetic import int_ceil_log as icl
 
-def test_online_appgrad(
-    ds1, ds2, cca_k):
-
-    model = AGCCA(cca_k, online=True)
-
-    model.fit(
-        ds1, ds2,
-        verbose=True)
-
-    return model.get_bases()
-
-def test_online_n_view_appgrad(
-    ds_list, cca_k):
-
-    model = NVAGCCA(cca_k, len(ds_list), online=True)
-
-    model.fit(
-        ds_list,
-        verbose=True)
-
-    return model.get_bases()
-
-def test_two_fixed_rate_scalar(
+def run_online_appgrad_e4_data_experiment(
     dir_path, file1, file2, cca_k,
     seconds=10,
     reader1=readers.get_scalar, 
-    reader2=readers.get_scalar):
+    reader2=readers.get_scalar,
+    exp=False, lower=None, verbose=False):
 
     bs = cca_k + icl(cca_k)
-    dl1 = FRL(dir_path, file1, seconds, reader1, online=True)
-    dl2 = FRL(dir_path, file2, seconds, reader2, online=True)
+    dl1 = None 
+    
+    if 'IBI' in file1:
+        dl1 = IBI(
+            dir_path, file1, seconds, reader1, online=True)
+    else:
+        dl1 = FRL(
+            dir_path, file1, seconds, reader1, online=True)
+
+    dl2 = None 
+    
+    if 'IBI' in file1:
+        dl2 = IBI(
+            dir_path, file2, seconds, reader2, online=True)
+    else:
+        dl2 = FRL(
+            dir_path, file2, seconds, reader2, online=True)
+
     ds1 = M2M(dl1, bs)
     ds2 = M2M(dl2, bs)
 
-    (Phi, unn_Phi, Psi, unn_Psi) = test_online_appgrad(
-        ds1, ds2, cca_k)
+    return run_online_appgrad_experiment(
+        ds1, ds2, cca_k,
+        exp=exp, lower=lower, verbose=verbose)
 
-    return (Phi, Psi)
-
-def test_n_fixed_rate_scalar(
+def run_n_view_online_appgrad_e4_data_experiment(
     dir_path, cca_k,
-    seconds=10,
-    weights=None):
+    seconds=10, exp=False, lower=None, verbose=False):
 
     bs = cca_k + icl(cca_k)
     mag = readers.get_magnitude
@@ -62,7 +55,7 @@ def test_n_fixed_rate_scalar(
         FRL(dir_path, 'HR.csv', seconds, sca, 1.0, online=True),
         FRL(dir_path, 'EDA.csv', seconds, sca, 4.0, online=True)]
     dss = [M2M(dl, bs) for dl in dls]
-    (basis_pairs, Psi) = test_online_n_view_appgrad(
-        dss, cca_k)
 
-    return (basis_pairs, Psi)
+    return run_online_n_view_appgrad_experiment(
+        dss, cca_k,
+        exp=exp, lower=lower, verbose=verbose)
--- src/python/experiments/random/online.py
+++ src/python/experiments/random/online.py
@@ -10,19 +10,11 @@ def run_online_appgrad_random_data_experiment(
     p1, p2, k, exp=False, lower=None, verbose=False):
 
     bs = k + icl(k)
-
-    print "Parameters:\n\t", "\n\t".join([
-        "batch_size: " + str(bs),
-        "p1: " + str(p1),
-        "p2: " + str(p2),
-        "k: " + str(k)])
-
     X_loader = GL(10*p1, p1)
     Y_loader = GL(10*p2, p2)
     X_server = B2M(X_loader, bs)
     Y_server = B2M(Y_loader, bs)
 
-    print "Testing CCA with boxcar-weighted Gram matrices"
     return aeu.run_online_appgrad_experiment(
         X_server, Y_server, k,
         exp=exp, lower=lower, verbose=verbose)
@@ -30,16 +22,10 @@ def run_online_appgrad_random_data_experiment(
 def run_n_view_online_appgrad_random_data_experiment(
     ps, k, exp=False, lower=None, verbose=False):
 
-    print "Gaussian random data online AppGrad CCA tests"
-    print "Parameters:\n\t", "\n\t".join([
-        "ps: " + str(ps),
-        "k: " + str(k)])
-
     bs = k + icl(k)
     loaders = [GL(10*p, p) for p in ps]
     servers = [B2M(loader, bs) for loader in loaders]
 
-    print "Testing n-view CCA with boxcar-weighted Gram matrices"
     return aeu.run_online_n_view_appgrad_experiment(
         servers, k,
         exp=exp, lower=lower, verbose=verbose)
