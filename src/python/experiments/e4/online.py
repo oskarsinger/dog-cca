fe39ed2e14f0cda094a76f0eb743c230b9e4e8de
oskarsinger <oskarsinger@gmail.com>
2016-06-14 12:25:24 -0400

    New experiment func

--- src/python/experiments/e4/online.py
+++ src/python/experiments/e4/online.py
@@ -8,6 +8,7 @@ from data.servers.minibatch import Minibatch2Minibatch as M2M
 from drrobert.arithmetic import int_ceil_log as icl
 
 import experiments.utils as eu
+import h5py
 
 def run_online_appgrad_e4_data_experiment(
     hdf5_path, cca_k, subject, 
@@ -74,3 +75,28 @@ def run_n_view_online_appgrad_e4_data_experiment(
     return eu.run_online_n_view_appgrad_experiment(
         dss, cca_k,
         exp=exp, lowers=lowers, verbose=verbose, etas=etas)
+
+def run_all_subject_n_view_online_appgrad_e4_data_experiment(
+    hdf5_path, cca_k,
+    seconds=10,
+    exp=False, verbose=False, pca_ks=[None]*6,
+    etas=None, lowers=None):
+
+    subjects = h5py.File(hdf5_path).keys()
+    models = []
+
+    for subject in subjects:
+        print 'Training model for subject', subject + '.'
+
+        model = run_n_view_online_appgrad_e4_data_experiment(
+            hdf5_path, cca_k, subject, 
+            seconds=seconds,
+            exp=exp, 
+            verbose=verbose, 
+            pca_ks=pca_ks, 
+            etas=etas, 
+            lowers=lowers)
+
+        models.append(model)
+
+    return models
