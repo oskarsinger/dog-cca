d1d7ff1e500cd37eba9bbd0181e656eea0965710
oskarsinger <oskarsinger@gmail.com>
2016-06-21 14:06:57 -0400

    More plotting and plots

--- src/python/experiments/plotting/bases.py
+++ src/python/experiments/plotting/bases.py
@@ -4,21 +4,17 @@ import numpy as np
 
 from math import log
 
-from lazyprojector import plot_matrix_heat, plot_lines
+from lazyprojector import plot_matrix_heat
 from drrobert.file_io import get_timestamped as get_ts
 from drrobert.misc import unzip
 
 from bokeh.plotting import figure, show, output_file, vplot
 from bokeh.palettes import GnBu9, YlOrRd9, BuPu9
 
-def plot_canonical_bases(Phis, Psi=None, plot_path='.'):
+def plot_canonical_bases(model, plot_path='.'):
 
+    Phis = unzip(model.get_bases())[1]
     (ps, ks) = unzip([Phi.shape for Phi in Phis])
-    (p_Psi, k_Psi) = (0, 0)
-
-    if Psi is not None:
-        p_Psi, k_Psi = Psi.shape
-        ks = ks + [k_Psi]
 
     k = None
     
@@ -31,23 +27,14 @@ def plot_canonical_bases(Phis, Psi=None, plot_path='.'):
     Phis_features = [[str(i) for i in range(p)]
                      for p in ps]
     basis_elements = [str(i) for i in range(k)]
-
     Phis_ps = [_plot_basis(Phi, 'Phi' + str(i), f, basis_elements)
                for i, (Phi, f) in enumerate(zip(Phis, Phis_features))]
-    Psi_p = None
 
-    if Psi is not None:
-        Psi_features = [str(i) for i in range(p_Psi)]
-        Psi_p = _plot_basis(Psi, 'Psi', Psi_features, basis_elements)
-    
     prefix = str(k) + '_k_' + \
         '_'.join([str(p) for p in ps]) + '_phis_'
     filename = get_ts(prefix + 
         'mass_per_feature_over_bases_matrix_heat_plot') + '.html'
     filepath = os.path.join(plot_path, filename)
-
-    if Psi is not None:
-        Phis_ps = Phis_ps + [Psi_p]
 
     output_file(
         filepath, 
