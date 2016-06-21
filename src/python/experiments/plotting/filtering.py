0f68801093f4acfcc709d0a794de46d590c40bc1
oskarsinger <oskarsinger@gmail.com>
2016-06-21 14:06:57 -0400

    More plotting and plots

--- src/python/experiments/plotting/filtering.py
+++ src/python/experiments/plotting/filtering.py
@@ -9,21 +9,26 @@ from bokeh.plotting import figure, show, output_file, vplot
 from bokeh.palettes import Spectral11
 
 def plot_cca_filtering(
-    filtered_Xs,
+    model,
     X_width=900,
     X_height=400,
     Y_width=900,
     Y_hieght=400,
     plot_path='.'):
 
+    filtered_Xs = model.get_status()['filtering_history']
     shapes = [X.shape for X in filtered_Xs] 
+    k = None
 
-    if not all([shapes[0][1] == shape[1] for shape in shapes]):
+    if len(set([shape[1] for shape in shapes])) == 1:
+        k = shapes[0][1]
+    else:
         raise ValueError(
-            'filtered_Xs should all have same first dimension.')
+            'Filter sequences should all have same first dimension.')
 
-    k = shapes[0][1]
     X_plots = []
+    X_label = 'Time Step Observed'
+    Y_label = 'Filtered Data Point for View '
 
     for (i, X) in enumerate(filtered_Xs):
         X_map = {'X filter dimension ' + str(i) : 
@@ -31,9 +36,9 @@ def plot_cca_filtering(
                  for i in xrange(k)}
         X_plots.append(plot_lines(
             X_map,
-            "Time Step",
-            "Filtered Data Point for View " + i,
-            "Filtered Data Points for View " + i + " vs Time Step Observed",
+            X_label,
+            Y_label + str(i),
+            Y_label + str(i) + ' vs ' + X_label,
             colors=Spectral11[:4]+Spectral11[-4:],
             width=X_width,
             height=X_height))
