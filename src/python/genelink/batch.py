8addba06b2ad9fd2a33dc846e9299b1264ce8a5e
oskarsinger <oskarsinger@gmail.com>
2016-06-30 15:24:02 -0400

    Updates

--- src/python/genelink/batch.py
+++ src/python/genelink/batch.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 from linal.qr import get_q
-from linal.utils import multi_dot
-from linal.utils import quadratic as quad
+from linal.utils import multi_dot, quadratic as quad
 from linal.svd_funcs import get_svd_power
 from optimization.utils import is_converged
 from optimization.optimizers.ftprl import MatrixAdaGrad as MAG
--- src/python/genelink/online.py
+++ src/python/genelink/online.py
@@ -1,5 +1,20 @@
+import numpy as np
+
+from linal.qr import get_q
+from linal.utils import multi_dot, quadratic as quad
+from linal.svd_funcs import get_svd_power
+from optimization.utils import is_converged
+from optimization.optimizers.ftprl import MatrixAdaGrad as MAG
+
 class OnlineGenELinKSubroutine:
 
-    def __init__(self):
+    def __init__(self,
+        k, epsilon=10**(-5)):
 
+        self.k
+        self.epsilon = epsilon
+        self.W = None
+
+    def get_update(self, A, B): 
+        
         print "Stuff"
