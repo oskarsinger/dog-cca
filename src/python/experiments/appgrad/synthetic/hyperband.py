a39c4e148b8ce489617a17b94b6386c4c325fc74
oskarsinger <oskarsinger@gmail.com>
2016-09-03 15:16:06 -0400

    new stuff

--- src/python/experiments/appgrad/synthetic/hyperband.py
+++ src/python/experiments/appgrad/synthetic/hyperband.py
@@ -3,7 +3,8 @@ import numpy.random as npr
 import data.loaders.synthetic.shortcuts as dlss
 
 from runners.bandit import FiniteHyperBandRunner as FHBR
-from arms.appgrad import NViewAppGradCCA as NVAGCCA
+from appgrad import NViewAppGradCCA as NVAGCCA
+from arms.appgrad import NViewAppGradCCAArm as NVAGCCAA
 from data.servers.minibatch import Minibatch2Minibatch as M2M
 from data.servers.gram import ExpGramServer as EGS
 from data.servers.gram import BoxcarGramServer as BCGS
@@ -11,15 +12,14 @@ from drrobert.arithmetic import int_ceil_log as icl
 from drrobert.random import log_uniform as lu
 from optimization.optimizers.quasinewton import FullAdamOptimizer as FADO
 from optimization.stepsize import InverseSquareRootScheduler as ISRS
-from .. import utils as eau
 
 def run_n_view_online_appgrad_cosine_data_hyperband_experiment(
     k, n, ps, 
     periods, amplitudes, phases, indexes,
-    max_rounds,
-    max_size,
-    min_size):
+    max_rounds=10):
 
+    max_size = n
+    min_size = int(min(periods)) * 3
     dl_list = dlss.get_cosine_loaders(
         ps, periods, amplitudes, phases, indexes,
         period_noise, phase_noise, amplitude_noise)
