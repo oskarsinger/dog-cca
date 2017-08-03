9e0d17187f47dd1dfb15fd7d0e09237ed45ab227
oskar <oskarsinger@gmail.com>
2017-08-03 02:42:56 -0400

    Better linal, fewer operations

--- dogcca/appgrad/node.py
+++ dogcca/appgrad/node.py
@@ -41,6 +41,7 @@ class AppGradView:
         num_views,
         k, 
         idn, 
+        gram_reg=10**(-5),
         batch_server=None,
         qn_server=None,
         eta_server=None):
@@ -49,9 +50,13 @@ class AppGradView:
         self.k = k
         self.idk = idn
 
+        self.batch_size = 2 * self.k
+
         # TODO: consider making it bigger than k for conditioning purposes
         if batch_server is None:
-            batch_server = PQ(2*self.k)
+            batch_server = PQ(
+                self.batch_size, 
+                center=True)
 
         self.batch_server = batch_server
 
@@ -66,8 +71,8 @@ class AppGradView:
         self.eta_server = eta_server
 
         self.neighbor_state = [None] * self.num_views
-        self.gram_server = BGS(reg=10**(-12))
         self.unn_Phi = None
+        self.X_unn_Phi = None
         self.Phi = None
         self.d = None
         self.data = None
@@ -78,11 +83,12 @@ class AppGradView:
 
         self.batch_server.add_data(data)
         self.batch = self.batch_server.get_batch()
-        self.gram = self.gram_server.get_gram(self.batch)
+        self.batch /= np.sqrt(self.batch_size)
 
         if self.num_rounds == 0:
             self.d = data.shape[1]
             self.unn_Phi = np.random.randn(self.d, self.k)
+            self.X_unn_Phi = np.dot(self.batch, self.unn_Phi)
 
             self._update_Phi()
 
@@ -117,16 +123,17 @@ class AppGradView:
             self.batch.T, 
             neighbor_sum)
         self_term = self.num_views * np.dot(
-            self.gram, self.unn_Phi)
+            self.batch.T, self.X_unn_Phi)
         gradient = self_term - neighbor_term
         qn_transd = self.qns.get_transform(gradient)
         eta = self.eta_server.get_stepsize()
 
         self.unn_Phi -= eta * qn_transd
+        self.X_unn_Phi = np.dot(self.batch, self.unn_Phi)
             
     def _update_Phi(self):
 
-        pre_sqrt = get_quadratic(self.unn_Phi, self.gram)
+        pre_sqrt = np.dot(self.X_unn_Phi.T, self.X_unn_Phi)
         pre_sqrt_norm = np.linalg.norm(pre_sqrt)
         normalizer = get_svd_power(pre_sqrt, -0.5)
         
